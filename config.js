9ceb1bd98cb1004c8002357807d42b2ff8db1ea7
John Klingler <john.klingler@monsanto.com>
2015-08-12 15:17:45 -0500

    Disable legacy namespacing.

--- config.js
+++ config.js
@@ -2,6 +2,9 @@
     return {
         graphitePort: parseInt(process.env.GRAPHITE_PORT) || 2003,
         graphiteHost: process.env.GRAPHITE_HOST || "localhost",
-        port: parseInt(process.env.STATSD_PORT) || 8125
+        port: parseInt(process.env.STATSD_PORT) || 8125,
+        graphite: {
+          legacyNamespace: false
+        }
     };
 })()
